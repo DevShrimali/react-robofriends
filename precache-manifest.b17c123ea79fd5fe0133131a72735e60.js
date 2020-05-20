d21b9233056f5170def10ad8471c7f2d13bd99a6
Dev Shrimali <devloper.ds@gmail.com>
2020-05-20 20:14:25 +0530

    Updates

---
+++ precache-manifest.b17c123ea79fd5fe0133131a72735e60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d1be6625074ba155eb055c6fa8a036",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/react-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e3fa9efdbf42f501e1cf",
+    "url": "/react-robofriends/static/css/main.e35f79b2.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/react-robofriends/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robofriends/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3fa9efdbf42f501e1cf",
+    "url": "/react-robofriends/static/js/main.fc64cca2.chunk.js"
+  },
+  {
+    "revision": "91e8b2a67cd29869f165",
+    "url": "/react-robofriends/static/js/runtime-main.aef47920.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/react-robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
